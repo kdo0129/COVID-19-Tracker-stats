562b243e9602ab2ebe4585a303ee3f4645ae3685
kdo0129 <eha0129@naver.com>
2020-05-19 15:40:17 +0900

    Make Cards and fetch data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import { Chart, Cards, CountryPicker } from './components';
+import { fetchData } from './api';
+
+import styles from './App.module.css';
 
 class App extends Component {
+	state = {
+		data: {},
+	};
+
+	async componentDidMount() {
+		const data = await fetchData();
+		this.setState({ ...this.state, data });
+	}
+
 	render() {
+		const { data } = this.state;
 		return (
-			<div>
-				<Cards />
+			<div className={styles.container}>
+				<Cards data={data} />
 				<CountryPicker />
 				<Chart />
 			</div>
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,87 @@
 import React from 'react';
+import { Card, CardContent, Typography, Grid } from '@material-ui/core';
+import cx from 'classnames';
+import CountUp from 'react-countup';
 
-const Cards = () => {
+import styles from './Cards.module.css';
+
+const Cards = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+	if (!confirmed) {
+		return 'Loading.....';
+	}
+
+	const updateDate = new Date(lastUpdate).toDateString();
 	return (
-		<>
-			<h1>Cards!</h1>
-		</>
+		<div className={styles.container}>
+			<Grid container spacing={3} justify="center">
+				<Grid
+					item
+					md={3}
+					component={Card}
+					className={cx(styles.card, styles.infected)}
+				>
+					<CardContent>
+						<Typography color="textPrimary">확진자 수</Typography>
+						<Typography variant="h5" component="h2">
+							<CountUp
+								start={0}
+								end={confirmed.value}
+								duration={2.5}
+								separator={','}
+							/>
+						</Typography>
+						<Typography color="textSecondary">{updateDate}</Typography>
+						<Typography variant="body2" component="p">
+							Number of active case of COVID-19
+						</Typography>
+					</CardContent>
+				</Grid>
+				<Grid
+					item
+					md={3}
+					component={Card}
+					className={cx(styles.card, styles.recovered)}
+				>
+					<CardContent>
+						<Typography color="textPrimary">완치자 수</Typography>
+						<Typography variant="h5" component="h2">
+							<CountUp
+								start={0}
+								end={recovered.value}
+								duration={2.5}
+								separator={','}
+							/>
+						</Typography>
+						<Typography color="textSecondary">{updateDate}</Typography>
+						<Typography variant="body2" component="p">
+							Number of recoveries from COVID-19
+						</Typography>
+					</CardContent>
+				</Grid>
+				<Grid
+					item
+					md={3}
+					component={Card}
+					className={cx(styles.card, styles.deaths)}
+				>
+					<CardContent>
+						<Typography color="textPrimary">사망자 수</Typography>
+						<Typography variant="h5" component="h2">
+							<CountUp
+								start={0}
+								end={deaths.value}
+								duration={2.5}
+								separator={','}
+							/>
+						</Typography>
+						<Typography color="textSecondary">{updateDate}</Typography>
+						<Typography variant="body2" component="p">
+							Number of deaths causedd by COVID-19
+						</Typography>
+					</CardContent>
+				</Grid>
+			</Grid>
+		</div>
 	);
 };
 
